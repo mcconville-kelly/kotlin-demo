163509c6316eeb27df503baa4b9e6e89d8175875
McConville, Kelly <McConville.Kelly@principal.com>
2019-07-29 11:57:18 -0500

    initial commit

---
+++ src/main/kotlin/com/principal/com/kotlindemo/KotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.principal.com.kotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDemoApplication>(*args)
+}
